70c950e98d6ef296d0a965d90e543086c9256ad6
aurelienmay <48348880+aurelienmay@users.noreply.github.com>
2020-03-16 09:45:44 +0100

    Modificaitons.

--- app/src/main/java/com/example/berclazmayskiseller/ui/products/ProductsFragment.java
+++ app/src/main/java/com/example/berclazmayskiseller/ui/products/ProductsFragment.java
@@ -7,59 +7,38 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 
 import com.example.berclazmayskiseller.R;
 import com.example.berclazmayskiseller.ui.home.HomeFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ProductsFragment extends Fragment implements View.OnClickListener {
-
-//    private ProductsViewModel productsViewModel;
-//
-//    public View onCreateView(@NonNull LayoutInflater inflater,
-//                             ViewGroup container, Bundle savedInstanceState) {
-//        productsViewModel =
-//                ViewModelProviders.of(this).get(ProductsViewModel.class);
-//        View root = inflater.inflate(R.layout.fragment_products, container, false);
-//        final TextView textView = root.findViewById(R.id.text_products);
-//        productsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
-//
-//        return root;
-//    }
 
     Button myButton;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_products, container, false);
-
         View myView = inflater.inflate(R.layout.fragment_products, container, false);
         myButton = (Button) myView.findViewById(R.id.button_displayProducts);
         myButton.setOnClickListener(this);
+
         return myView;
     }
 
-//    @Override
-//    public void onClick(View v) {
-//        Toast.makeText(getActivity(),"Products displayed !",Toast.LENGTH_SHORT).show();
-//        Intent intent = new Intent(getActivity(), DisplayProductsActivity.class);
-//        startActivity(intent);
-//    }
-
     @Override
     public void onClick(View v) {
-        HomeFragment fragment2 = new HomeFragment();
-        FragmentManager fragmentManager = getFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.container, fragment2);
+        HomeFragment homeFragment = new HomeFragment();
+        FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.navigation_home, homeFragment);
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
     }
