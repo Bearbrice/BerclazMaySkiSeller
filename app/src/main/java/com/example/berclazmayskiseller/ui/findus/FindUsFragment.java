b02cc7ee9b6fe7be6e190d0b1b0f077d29b6a313
aurelienmay <48348880+aurelienmay@users.noreply.github.com>
2020-03-18 16:10:57 +0100

    Deletion of Anzere and Verbier fragment. Reformat Findus fragment. Settings's switcher saved after shut down.

--- app/src/main/java/com/example/berclazmayskiseller/ui/findus/FindUsFragment.java
+++ app/src/main/java/com/example/berclazmayskiseller/ui/findus/FindUsFragment.java
@@ -1,54 +1,119 @@
 package com.example.berclazmayskiseller.ui.findus;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
-import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 import com.example.berclazmayskiseller.R;
-import com.example.berclazmayskiseller.ui.findus.map.AnzereFragment;
-import com.example.berclazmayskiseller.ui.findus.map.VerbierFragment;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class FindUsFragment extends Fragment {
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+    GoogleMap mMap;
+
+    public View onCreateView(final LayoutInflater inflater,
+                             final ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_findus, container, false);
 
-        Button button_verbier = (Button) view.findViewById(R.id.button_verbier);
-        Button button_anzere = (Button) view.findViewById(R.id.button_anzere);
+        Button button_verbier = view.findViewById(R.id.button_verbier);
+        Button button_anzere = view.findViewById(R.id.button_anzere);
 
+        //VERBIER BTN SETTINGS
         button_verbier.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                addFragment(new VerbierFragment(), false, "one");
+
+                SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.container_frame_back);  //use SuppoprtMapFragment for using in fragment instead of activity  MapFragment = activity   SupportMapFragment = fragment
+                mapFragment.getMapAsync(new OnMapReadyCallback() {
+                    @Override
+                    public void onMapReady(GoogleMap mMap) {
+                        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+                        mMap.clear(); //clear old markers
+
+                        //Smooth zoom, angle,...
+                        CameraPosition googlePlex = CameraPosition.builder()
+                                .target(new LatLng(46.093634, 7.233007))
+                                .zoom(15)
+                                .bearing(0)
+                                .tilt(0)
+                                .build();
+
+                        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(googlePlex), 3000, null);
+
+                        Marker marker = mMap.addMarker(new MarkerOptions()
+                                .position(new LatLng(46.093634, 7.233007))
+                                .title("SkiSeller Verbier")
+                                .snippet("079 328 65 48")
+                                .icon(bitmapDescriptorFromVector(getActivity(), R.drawable.ic_products_black_24dp)));
+                        marker.showInfoWindow();
+                    }
+                });
             }
         });
 
+        //ANZERE BTN SETTINGS
         button_anzere.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                addFragment(new AnzereFragment(), false, "one");
+
+                SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.container_frame_back);  //use SuppoprtMapFragment for using in fragment instead of activity  MapFragment = activity   SupportMapFragment = fragment
+                mapFragment.getMapAsync(new OnMapReadyCallback() {
+                    @Override
+                    public void onMapReady(GoogleMap mMap) {
+                        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+                        mMap.clear(); //clear old markers
+
+                        //Smooth zoom, angle,...
+                        CameraPosition googlePlex = CameraPosition.builder()
+                                .target(new LatLng(46.294862, 7.395471))
+                                .zoom(15)
+                                .bearing(0)
+                                .tilt(0)
+                                .build();
+
+                        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(googlePlex), 3000, null);
+
+                        Marker marker = mMap.addMarker(new MarkerOptions()
+                                .position(new LatLng(46.294862, 7.395471))
+                                .title("SkiSeller Anzère")
+                                .snippet("079 328 65 48")
+                                .icon(bitmapDescriptorFromVector(getActivity(), R.drawable.ic_products_black_24dp)));
+                        marker.showInfoWindow();
+                    }
+                });
             }
         });
 
         return view;
     }
 
-    public void addFragment(Fragment fragment, boolean addToBackStack, String tag) {
-        FragmentManager manager = getActivity().getSupportFragmentManager();
-        FragmentTransaction ft = manager.beginTransaction();
-
-        if (addToBackStack) {
-            ft.addToBackStack(tag);
-        }
-        ft.replace(R.id.container_frame_back, fragment, tag);
-        ft.commitAllowingStateLoss();
+    //MAP MARKER SETTINGS
+    private BitmapDescriptor bitmapDescriptorFromVector(Context context, int vectorResId) {
+        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
+        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        vectorDrawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
     }
 }
--- app/src/main/java/com/example/berclazmayskiseller/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/berclazmayskiseller/ui/settings/SettingsFragment.java
@@ -1,24 +1,151 @@
 package com.example.berclazmayskiseller.ui.settings;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.SpannableStringBuilder;
+import android.text.style.RelativeSizeSpan;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 
+import com.example.berclazmayskiseller.MainActivity;
 import com.example.berclazmayskiseller.R;
 
 public class SettingsFragment extends Fragment {
 
+    private String CHANNEL_ID = "test_notification  ";
+
     public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_settings, container, false);
+                             ViewGroup container, final Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
 
-        return root;
+        //Get shared preferences for switches
+        SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+
+        //Switch creator
+        Switch notifications = view.findViewById(R.id.switch_notifications);
+        //Set switch state
+        boolean notifications_defaultValue = getResources().getBoolean(R.bool.notifications_default_value);
+        boolean notifications_switchSaverIsChecked = sharedPref.getBoolean(String.valueOf(R.bool.notifications_checked), notifications_defaultValue);
+        notifications.setChecked(notifications_switchSaverIsChecked);
+        //Set switch listener
+        notifications.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                //Save switch state
+                SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putBoolean(String.valueOf(R.bool.notifications_checked), isChecked);
+                editor.commit();
+            }
+        });
+
+        //Switch creator
+        Switch position = view.findViewById(R.id.switch_position);
+        //Set switch state
+        boolean position_defaultValue = getResources().getBoolean(R.bool.position_default_value);
+        boolean position_switchSaverIsChecked = sharedPref.getBoolean(String.valueOf(R.bool.position_checked), position_defaultValue);
+        position.setChecked(position_switchSaverIsChecked);
+        //Set switch listener
+        position.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                //Save switch state
+                SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putBoolean(String.valueOf(R.bool.position_checked), isChecked);
+                editor.commit();
+            }
+        });
+
+        //Switch creator
+        final Switch newsletter = view.findViewById(R.id.switch_newsletter);
+        //Set switch state
+        boolean newsletter_defaultValue = getResources().getBoolean(R.bool.newsletter_default_value);
+        boolean newsletter_switchSaverIsChecked = sharedPref.getBoolean(String.valueOf(R.bool.newsletter_checked), newsletter_defaultValue);
+        newsletter.setChecked(newsletter_switchSaverIsChecked);
+        //Set switch listener
+        newsletter.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                //Save switch state
+                SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putBoolean(String.valueOf(R.bool.newsletter_checked), isChecked);
+                editor.commit();
+
+                if (isChecked) {
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                    builder.setTitle("Enter your email address");
+
+                    // Set up the input
+                    final EditText input = new EditText(getContext());
+                    // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+                    input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+                    builder.setView(input);
+
+                    // Set up the buttons
+                    builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            String emailAddressEntered = "";
+                            emailAddressEntered = input.getText().toString();
+
+                            String confirmationMessage = "Confirmation sended to " + emailAddressEntered;
+                            SpannableStringBuilder biggerConfirmationMessage = new SpannableStringBuilder(confirmationMessage);
+                            biggerConfirmationMessage.setSpan(new RelativeSizeSpan(1.35f), 0, confirmationMessage.length(), 0);
+                            Toast confirmationToast = Toast.makeText(getContext(), biggerConfirmationMessage, Toast.LENGTH_LONG);
+                            confirmationToast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL, 0, 0);
+
+                            String errorMessage = emailAddressEntered + " is not a valid mail.";
+                            SpannableStringBuilder biggerErrorMessage = new SpannableStringBuilder(errorMessage);
+                            biggerErrorMessage.setSpan(new RelativeSizeSpan(1.35f), 0, errorMessage.length(), 0);
+                            Toast wrongMailToast = Toast.makeText(getContext(), biggerErrorMessage, Toast.LENGTH_LONG);
+                            wrongMailToast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL, 0, 0);
+
+                            //SEND MAIL ALGO
+                            //TOAST WILL BE SEND ACCORDING TO EXCEPTION OF THE MAIL
+
+                            if (emailAddressEntered.contains("@")) {
+                                confirmationToast.show();
+                            } else {
+                                wrongMailToast.show();
+                                newsletter.setChecked(false);
+                            }
+                        }
+                    });
+
+                    builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            newsletter.setChecked(false);
+                            dialog.cancel();
+                        }
+                    });
+
+                    builder.show();
+                }
+
+
+            }
+        });
+
+        return view;
     }
-
-
 }
